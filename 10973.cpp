7ba4bd21b3c3436e3364554a6caf643d09096a5e
JinSY515 <73789740+JinSY515@users.noreply.github.com>
2021-02-08 20:23:16 +0900

    Create 10973.cpp
    백준 10973번 문제 풀이 코드. 이전 순열 prev_permutation 이용

---
+++ 10973.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int main(void){
+  int arr[10001];
+  int i,n;
+  scanf("%d",&n);
+  vector<int> v(n);
+  for(i=0; i<n; i++){
+    scanf("%d",&v[i]);
+  }
+  if(prev_permutation(v.begin(),v.end())){
+    for(int i=0; i<n; i++){
+      printf("%d ",v[i]);
+    }
+  }
+  else printf("-1");
+  return 0;
+}
